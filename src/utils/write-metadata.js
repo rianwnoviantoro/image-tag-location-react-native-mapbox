d6822c29e049e16b9836320bec08b3fa32677ebe
rianwnoviantoro <tenkeylost@gmail.com>
2023-08-01 15:50:50 +0700

    refactor

--- src/utils/write-metadata.js
+++ src/utils/write-metadata.js
@@ -5,8 +5,6 @@ export default writeMetadata = async (base64, location) => {
   let data = Buffer.from(base64, "base64").toString("binary");
   let exif = piexif.load(data);
 
-  console.log("Before: ", exif.GPS);
-
   exif.GPS[piexif.GPSIFD.GPSLatitude] = piexif.GPSHelper.degToDmsRational(
     location.latitude
   );
@@ -14,15 +12,11 @@ export default writeMetadata = async (base64, location) => {
     location.longitude
   );
 
-  console.log("Inject: ", exif.GPS);
-
   let bytes = piexif.dump(exif);
 
   const result = piexif.insert(bytes, data);
 
   let show = piexif.load(result);
-
-  console.log("After: ", show.GPS);
 
   return result;
 };
